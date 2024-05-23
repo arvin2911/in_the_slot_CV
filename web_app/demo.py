40920b1f58819407e3ba3b53107d5fd41a3cca59
SamanthaReksosamudra <samantha_reksosamudra@yahoo.com>
2024-05-23 14:44:42 -0700

    fully integrated (i hope)

--- web_app/demo.py
+++ web_app/demo.py
@@ -1,4 +1,6 @@
 # import libraries
+import modules
+
 import cv2
 import numpy as np
 from scipy.spatial.distance import pdist, squareform
@@ -17,63 +19,6 @@ import dill
 import ipywidgets as widgets
 import sys
 import os
-
-# %matplotlib notebook # enables interactive mode in Jupyter Notebook
-
-# Alternative for Python script to enable interactive plotting:
-plt.ion()
-
-# import calibration matrix
-#TODO:add the script to the folder
-mtx = np.load("Andrew_camera_matrix.npy")  
-fx = mtx[0][0]
-fy = mtx[1][1]
-cx = mtx[0][2]
-cy = mtx[1][2]
-
-# Load the model
-#TODO:add the model to the folder
-model = YOLO('bestwpingpong.pt') 
-
-cap = cv2.VideoCapture('Strike zone overlay/please_work/IMG_5296.MOV') #Read in pitch from folder
-frame_list = []  #List where we will store each frame of the video
-while(cap.isOpened()): 
-      
-# Capture frame-by-frame 
-    ret, frame = cap.read() 
-    if ret == True: 
-    # Display the resulting frame 
-#         cv2.imshow('Frame', frame) 
-        frame_list.append(frame)
-    # Press Q on keyboard to exit 
-        if cv2.waitKey(25) & 0xFF == ord('q'): 
-            break
-  
-
-    else: 
-        break
-
-# the video capture object 
-cap.release() 
-  
-# Closes all the frames 
-cv2.destroyAllWindows() 
-
-# Process the video and return the processed video's path
-def process_video(video_path):
-    # saved directory path
-    DEST_DIR = "static/results"
-    SAVED_DIR = "yoohoo"
-    filename = os.path.basename(video_path)
-
-    # Predict with model
-    results = model.predict(video_path, save=True, project=DEST_DIR, name=SAVED_DIR, exist_ok=True)
-    
-    return os.path.join(DEST_DIR,SAVED_DIR,filename)   # return the video path
-
-
-
-## DEFINE FUNCTIONS
 
 # Run calibration sequence
 def is_orange(image, x1, y1, x2, y2, lower_orange, upper_orange, color_threshold=0.5):
@@ -283,3 +228,240 @@ def order_homeplate_points(frame, points, pingpong_id=80):
     corners = determine_plate_corners(points, distance_matrix, front_tip_index)
     print("Identified corners:", corners)
     return corners,[points[corners[0]], points[corners[1]], points[corners[2]], points[corners[3]], points[corners[4]]]
+
+# Build the strike zone
+def build_zone(world_coordinate_list,expected_distances):
+    #The world coordinate list should be ordered as follows: The 1st entry should be the position of the ball that is at the back (the point) of the plate, from there, 
+    #rotate counterclockwise until every ball on the plate is accounted for. The sixth entry will be the lower bound for the zone, the seventh entry will be the upper bound of the zone
+    #We're also going to average over the coordinates of the corners of the zone to mitigate errors in strike zone dimensions
+# For the strike zone, the width of the plate from this perspective is the x coordinate (increasing from left to right), the length is the z coordinate (increasing from top to bottom of the screen
+#and the y coordinate is the height of the zone coming out of the screen. REMEMBER THIS
+     #/\
+    #|  |
+    #____
+    #Front of the zone
+    #Front plane of the zone length in x coordinate
+    front_plane_length_x = world_coordinate_list[2][0] - world_coordinate_list[3][0] 
+    back_plane_length_x = world_coordinate_list[1][0] - world_coordinate_list[4][0] 
+
+    x_length_front = ( front_plane_length_x + back_plane_length_x )/2 #average of the 2 lengths
+
+    #z length of the front zone. This is the depth of the plate
+
+    right_plane_length_z = (world_coordinate_list[2][2]) - (world_coordinate_list[1][2]) 
+    
+    left_plane_length_z = world_coordinate_list[4][2] - world_coordinate_list[3][2] 
+    print(right_plane_length_z)
+    print(left_plane_length_z)
+    z_length_front = max(np.abs(left_plane_length_z),np.abs(right_plane_length_z)) #average of the 2 lengths
+
+    y_bottom =  world_coordinate_list[5][1] #Height of the bottom of the zone
+    y_top = world_coordinate_list[6][1] #height of the top of the zone
+    y_length = y_top - y_bottom
+    #Now for the back of the zone where we have curvature. We want to return the slope of the rear of the zone so we can use that for ball and strike checking later
+    back_zone_left_edge_slope = (world_coordinate_list[0][2] - world_coordinate_list[4][2] )/( world_coordinate_list[0][0] - world_coordinate_list[4][0] ) #change in z over change in x babay
+
+    back_zone_right_edge_slope = (world_coordinate_list[0][2] - world_coordinate_list[1][2] )/( world_coordinate_list[0][0] - world_coordinate_list[1][0] ) #change in z over change in x babay
+    #Also want x intercepts of both of these lines
+
+    intercept_left = world_coordinate_list[0][2] - back_zone_left_edge_slope*world_coordinate_list[0][0] #calculate z intercept of the left slope of the zone
+    intercept_right = world_coordinate_list[0][2] - back_zone_right_edge_slope*world_coordinate_list[0][0] #calculate the z intercept of the right slope
+    #We also want the y dimension length of the back of the zone. So the tip of the plate to the back plane of the zone
+    z_length_back = max(  (np.abs(world_coordinate_list[0][2] - world_coordinate_list[4][2]) ), (np.abs(world_coordinate_list[0][2] - world_coordinate_list[1][2]) ) ) #average over 2 possible lengths for the 
+                                                                                                                                                    #length of the triangular part of the zone
+    #Going to return these lengths as a dictionary
+
+    zone_dim_dict = {
+        'x_length_front': x_length_front,
+        'y_bottom': y_bottom,
+        'y_top': y_top,
+        'y_length': y_length,
+        'z_length_front': z_length_front,
+        'z_length_back': z_length_back,
+        'back_zone_left_slope': back_zone_left_edge_slope,
+        'back_zone_right_slope': back_zone_right_edge_slope, 
+        'intercept_left': intercept_left,
+        'intercept_right': intercept_right
+    }
+
+    return zone_dim_dict # return a dictionary of the bounding edges of the zone
+
+# Check if baseball pitch is a strike 
+def check_strike(ball_position, zone_bounds):
+    #ball position: tuple of the world coordinates of the ball
+    #Zone bounds: list of tuples that contains the center positions of each of the balls used to mark the edges of the strike zone.
+    #The 1st entry should be the position of the ball that is at the back (the point) of the plate, from there, 
+    #rotate counter clockwise until every ball on the plate is accounted for. The sixth entry will be the lower bound for the zone, the seventh entry will be the upper bound of the zone
+    #We're also going to average over the coordinates of the corners of the zone to mitigate errors in strike zone dimensions
+    
+    zone_dims_dict = build_zone(zone_bounds)
+    
+    z_check_front = ball_position[2] >= zone_bounds[3][2] and ball_position[2] <= zone_bounds[3][2] + zone_dims_dict.get('z_length_front') #Check if ball is outside the front of zone to start 
+    if z_check_front == True: #Ball is in the front rectangular region of the zone
+        x_check_front = ball_position[0] >= zone_bounds[3][0] and ball_position[0] <= zone_bounds[3][0] + zone_dims_dict.get('x_length_front') #Check if ball is in x range of the front of the zone (width of the zone)
+        y_check_front = ball_position[1] >= zone_dims_dict.get('y_bottom') and ball_position[1] <= zone_dims_dict.get('y_top') #check if ball is in the height of zone 
+        if x_check_front == True and y_check_front == True:
+            strike = 1
+            return strike
+
+    z_check_back = ball_position[2] >= zone_bounds[4][2] and ball_position[2] <= zone_bounds[4][2] + zone_dims_dict.get('z_length_back') #check if ball z coordinate is in the back portion of the zone (triangular part)
+    # print(z_check_back)
+    if z_check_back == True:
+        #Using the x intercepts of the lines calculated in zone_dims 
+        x_check_back = ball_position[0] >= ( ball_position[2] - zone_dims_dict.get('intercept_left') ) / (zone_dims_dict.get('back_zone_left_slope')) \
+                        and ball_position[0] <= ( ball_position[2] - zone_dims_dict.get('intercept_right') ) / (zone_dims_dict.get('back_zone_right_slope'))
+        
+        y_check_back = ball_position[1] >= zone_dims_dict.get('y_bottom') and ball_position[1] <= zone_dims_dict.get('y_top')#check if ball is in the y range of the zone (the height)
+        # print(y_check_back)
+        if x_check_back == True and y_check_back == True:
+            strike = 1
+            return strike
+    strike = 0
+    return strike
+
+# Draw the prism of the strike zone overlay
+def draw_pentagonal_prism(ax, zone_bounds,zone_dims_dict,center=(0, 0, 0)):
+    # Define the vertices of a regular pentagon
+    size = 1
+    # vertices_base = np.array([
+    #      [size * np.cos(2 * np.pi * i / 5), size * np.sin(2 * np.pi * i / 5), 0] for i in range(5)
+    #  ])
+    vertices_base = np.array([
+     [zone_bounds[i][0], zone_bounds[i][2] + zone_dims_dict.get('y_bottom'), zone_bounds[i][1] ] for i in range(len(zone_bounds)-2) 
+    ])
+    height = zone_dims_dict.get('y_length') #top and bottom of the zone
+    # Extrude the pentagon to create the sides
+    
+    vertices_top = vertices_base + np.array([0, 0, height])
+    print(vertices_base)
+    print((vertices_top))
+    vertices = np.vstack([vertices_base, vertices_top])
+
+    # Create the faces of the prism
+    faces = [
+        [vertices_base[i], vertices_base[(i + 1) % 5], vertices_top[(i + 1) % 5], vertices_top[i]] for i in range(5)
+    ]
+    faces.append(vertices_base)
+    faces.append(vertices_top)
+
+    # Plot the prism
+    ax.add_collection3d(Poly3DCollection(faces, facecolors='cyan', linewidths=1, edgecolor = 'r', alpha = .6))
+
+
+
+## MAIN EXECUTION BLOCK
+
+# import calibration matrix
+#TODO:add the script to the folder
+mtx = np.load("Andrew_camera_matrix.npy")  
+fx = mtx[0][0]
+fy = mtx[1][1]
+cx = mtx[0][2]
+cy = mtx[1][2]
+
+# Load the model
+#TODO:add the model to the folder
+model = YOLO('bestwpingpong.pt') 
+
+cap = cv2.VideoCapture('Strike zone overlay/please_work/IMG_5296.MOV') #Read in pitch from folder
+frame_list = []  #List where we will store each frame of the video
+while(cap.isOpened()): 
+      
+# Capture frame-by-frame 
+    ret, frame = cap.read() 
+    if ret == True: 
+    # Display the resulting frame 
+#         cv2.imshow('Frame', frame) 
+        frame_list.append(frame)
+    # Press Q on keyboard to exit 
+        if cv2.waitKey(25) & 0xFF == ord('q'): 
+            break
+  
+
+    else: 
+        break
+
+# the video capture object 
+cap.release() 
+  
+# Closes all the frames 
+cv2.destroyAllWindows() 
+
+# Load in video and save an initial frame to be used in process_image
+test_image = frame_list[20]
+cv2.imwrite("Strike zone overlay/please_work/validation_image.png",test_image)
+
+points = process_image("Strike zone overlay/please_work/validation_image.png")
+# print(len(points))
+# print(points)
+
+# Reorder corner points of the strike zone based on homeplate
+corners, point_order = (order_homeplate_points(test_image, points))
+
+#need to append on a zone height
+point_order.append((0,10,0)) #bottom border of the zone
+point_order.append((0,30,0)) #top of the zone
+
+expected_distances = np.array([11.8, 17, 8.5]) #convert from inches to meters
+zone_dims_dict = build_zone(point_order,expected_distances)
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+height = zone_dims_dict.get('y_length')
+center = (point_order[0][0] ,point_order[0][2] + zone_dims_dict.get('z_length_back'),point_order[0][1] + zone_dims_dict.get('y_length')*.5  )
+print(center)
+draw_pentagonal_prism(ax = ax,zone_bounds=point_order,zone_dims_dict=zone_dims_dict,center=center)
+x_lim = zone_dims_dict.get('x_length_front') + center[0]
+y_lim = (zone_dims_dict.get('z_length_front'))+ (zone_dims_dict.get('z_length_back'))
+print(y_lim)
+# Set axis limits
+ax.set_xlim([-15, 5])
+ax.set_ylim([70, 90])
+ax.set_zlim([-2*height, 2*height])
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('z')
+#Create a legend with a color box
+strike_patch = mpatches.Patch(color='red', label='Strike')
+ball_patch = mpatches.Patch(color='blue', label='Ball')
+plt.legend(handles=[strike_patch, ball_patch], loc='upper left', framealpha=0.5, frameon=True)
+
+# for i in range(len(ball_positions)):
+#     if result[i] == 1:
+#         ax.scatter(ball_positions[i][0],ball_positions[i][2],ball_positions[i][1],c = 'r',s=200)
+#     else:
+#         ax.scatter(ball_positions[i][0],ball_positions[i][2],ball_positions[i][1],c = 'b',s=200)
+
+# Enable interactive mode
+plt.ion()
+
+# Show the plot
+plt.show()
+
+# Pause to allow user interaction
+plt.pause(0.1)
+
+# Disable interactive mode
+plt.ioff()
+
+# Display the plot
+plt.show()
+
+# plt.rcParams['savefig.facecolor']='white'
+# plt.savefig('3d_strikezone_image',bbox_inches="tight")
+
+# Process the video and return the processed video's path
+def process_video(video_path):
+    # saved directory path
+    DEST_DIR = "static/results"
+    SAVED_DIR = "yoohoo"
+    filename = os.path.basename(video_path)
+
+    # Predict with model
+    results = model.predict(video_path, save=True, project=DEST_DIR, name=SAVED_DIR, exist_ok=True)
+    
+    return os.path.join(DEST_DIR,SAVED_DIR,filename)   # return the video path
+
+
+
+
