a9923b28f644dea004768c23ba2c19730c1e47b1
arvin2911 <117593520+arvin2911@users.noreply.github.com>
2024-05-27 17:03:34 -0700

    Update app.py

--- web_app/app.py
+++ web_app/app.py
@@ -6,6 +6,7 @@ import os
 import subprocess
 
 import demo
+import EE399_MNIST
 
 # import pickle
 # import numpy as np
@@ -40,28 +41,34 @@ def home():
             #TODO: need to return the result.html with the interactive plot html in demo.py
             # Run the Python file with the uploaded video file as input
             # output = execute_python_script(file_path)
-            # output = demo.process_video(file_path)
+            # plot_path = EE399_MNIST.process_video(file_path)
+            # return render_template('result.html', plot_path=plot_path)
+
+            plot_path = demo.generate_result(file_path)
+            return render_template('result.html', plot_path=plot_path)
+
+             # Return the result page with the interactive plot
+            # return redirect(url_for('show_plot', filename=os.path.basename(plot_path)))
         
-            # return render_template('result.html', output=output)
         else:
             return "Invalid file format. Only video files allowed."
     return render_template('index.html', form=form)
 
 # checks if the file has the allowed extension or type
 def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'mp4', 'avi', 'mov', 'jpg', 'png'}
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'mp4', 'avi', 'mov'}
 
 # executes a python program using the file's path
-def execute_python_script(video_path):
-    try:
-        # Execute the Python script with the video file path as argument
-        output = subprocess.check_output(['python', 'models/demo.py', video_path], stderr=subprocess.STDOUT, universal_newlines=True)
-        # Assuming that the output of EE399_MNIST.py is the path to the generated picture or video
-        result_path = output.strip()  # Remove any leading/trailing whitespace
-        return result_path
+# def execute_python_script(video_path):
+#     try:
+#         # Execute the Python script with the video file path as argument
+#         output = subprocess.check_output(['python', 'models/demo.py', video_path], stderr=subprocess.STDOUT, universal_newlines=True)
+#         # Assuming that the output of EE399_MNIST.py is the path to the generated picture or video
+#         result_path = output.strip()  # Remove any leading/trailing whitespace
+#         return result_path
         
-    except subprocess.CalledProcessError as e:
-        return "Error: {}".format(e.output)
+#     except subprocess.CalledProcessError as e:
+#         return "Error: {}".format(e.output)
 
 
 # run the app
